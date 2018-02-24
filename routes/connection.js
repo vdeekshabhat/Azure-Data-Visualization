08c5987601466d91dbc9baa5160090c17ec8f81a
Deeksha Bhat <deekshabhat@Deekshas-MacBook-Pro.local>
2018-02-24 17:40:35 -0600

    modified

--- routes/connection.js
+++ routes/connection.js
@@ -1,7 +1,7 @@
 var mysql = require('mysql');
 
 var con = mysql.createConnection({
-  host: "assign3cloud.azurewebsites.net",
+  host: "localhost",
   user: "root",
   password: "deeksha",
   database: "sampledb",
