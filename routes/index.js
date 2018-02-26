3ff407854eca83ef9a9c5c23405863cb54436b15
Deeksha Bhat <deekshabhat@Deekshas-MacBook-Pro.local>
2018-02-26 11:59:40 -0600

    added

--- routes/index.js
+++ routes/index.js
@@ -11,18 +11,18 @@ router.get('/', function(req, res, next) {
     'age': 24
   };
   console.log('response = %o', response);
-  res.render('index', { name: response.name, age: response.age });
+  res.render('index', { name: response.name, age: response.age, rows: [] });
 });
 
 router.get('/loaddata', function(req, res, next) {
-  var query = "LOAD DATA INFILE '/var/lib/mysql-files/USZipcodes.csv' INTO TABLE USZipcodes FIELDS TERMINATED BY ',' IGNORE 1 LINES (zip,state,county,city);";
+  var query = "LOAD DATA INFILE '/var/lib/mysql-files/Starbucks.csv' INTO TABLE Starbucks FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '\"' LINES TERMINATED BY '\r\n' IGNORE 1 LINES (id,StarbucksId,Name,StoreNumber,PhoneNumber,Street1,Street2,Street3,city,CountrySubdivisionCode,CountryCode,PostalCode,@Longitude,@Latitude,Timezone)SET Longitude = nullif(@Longitude,' '),Latitude = nullif(@Latitude,' ');";
   start = new Date().getTime();
 
   connection.query(query, function(err, rows, fields) {
     end = new Date().getTime();
-    diff = end-start;
+    diff = (end-start)/1000 + 'sec';
     if (!err){
-      res.send('success'+diff);
+      res.send('success. The time taken for execution is: '+diff);
     }
     else{
       console.log('error %o',err);
@@ -30,4 +30,27 @@ router.get('/loaddata', function(req, res, next) {
     }
   });
 });
+
+router.post('/ziprange', function(req, res, next) {
+  var zipstart = req.body.zipstart;
+  var zipend = req.body.zipend;
+
+  var query = "Select * from USZipcodes where zip between "+zipstart+" and "+zipend+";";
+
+  start = new Date().getTime();
+
+  connection.query(query, function(err, rows, fields) {
+    end = new Date().getTime();
+    diff = (end-start)/1000 + 'sec';
+    if (!err){
+      res.render('index.pug',{rows: rows});
+    }
+    else{
+      console.log('error %o',err);
+      res.send('error '+diff);
+    }
+  });
+
+});
+
 module.exports = router;
