79fbf984b6fa6eab20f433c4934966032a0b80a2
Deeksha Bhat <deekshabhat@Deekshas-MacBook-Pro.local>
2018-03-07 00:53:21 -0600

    added

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     $('.submitbtn').click(function(){
         // Ajax call to get results from db
-        $.ajax({url: "/countrypiechart", success: function(result){
+        $.ajax({url: "/education", success: function(result){
             chartdata = [['State', 'Average']];
             for(var index in result){
                 row = result[index];
